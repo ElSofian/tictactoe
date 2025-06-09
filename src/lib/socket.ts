c736a72dac54884785d71e8c4a1d867cf831f2ad
Sofian <sofian.developpeur@gmail.com>
2025-06-09 21:01:19 +0200

    fix url for socket init

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,9 +2,11 @@ import { io, Socket } from 'socket.io-client';
 
 let socket: Socket | null = null;
 
+const url = process.env.NODE_ENV === 'production' ? 'https://ttt.sofianelaloui.me' : `http://localhost:${process.env.SOCKET_PORT}`;
+
 export function getSocket(): Socket {
   if (!socket) {
-    socket = io('http://localhost:3800', {
+    socket = io(url, {
       autoConnect: true,
       reconnection: false,
     });
